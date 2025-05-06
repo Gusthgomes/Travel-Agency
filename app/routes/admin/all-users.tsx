43934fdbb26d6654af060b661a19cd2739f3d8f5
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2025-05-06 20:59:44 -0300

    feat: admin routes created

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,10 @@
+const AllUsers = () => {
+    return (
+        <div className="flex flex-col gap-4 p-4">
+            <h1 className="text-2xl font-bold">All Users</h1>
+            <p>List of all users in the system.</p>
+        </div>
+    );
+}
+
+export default AllUsers;
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,11 @@
+
+const Dashboard = () => {
+    return (
+        <div className="flex flex-col gap-4 p-4">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p>Welcome to the admin dashboard!</p>
+        </div>
+    )
+}
+
+export default Dashboard
